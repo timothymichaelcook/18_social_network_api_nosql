ef820f86e00820e1a143d181250133b6b76415df
timothymichaelcook <tmc295@email.vccs.edu>
2023-04-12 21:25:12 -0400

    Fixed syntax error on line 5

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
 
-const app = express;
+const app = express();
 const PORT = process.env.PORT || 3001;
