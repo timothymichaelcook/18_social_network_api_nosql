c703d920a4b693deebfeb6cb08cbf986a3ee9a40
timothymichaelcook <tmc295@email.vccs.edu>
2023-04-12 22:01:58 -0400

    Coded userController object

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,17 @@
 const { User, Thought } = require("../models");
+
+const userController = {
+  getAllUser(req, res) {
+    User.find({})
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
