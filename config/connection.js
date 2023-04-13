bb416f68219ab21d054d2a9682e655e08e6f383f
timothymichaelcook <tmc295@email.vccs.edu>
2023-04-13 08:37:20 -0400

    Started connection.js file

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.set("debug", true);
+
+
