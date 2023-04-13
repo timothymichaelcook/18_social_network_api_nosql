63cc659322c55124b4986aa4b9965571b029acaa
timothymichaelcook <tmc295@email.vccs.edu>
2023-04-13 07:27:20 -0400

    Coded update user function with body and response arguments

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -15,7 +15,6 @@ const userController = {
         res.sendStatus(400);
       });
   },
-
   getUserById({ params }, res) {
     User.findOne({ _id: params.id })
       .populate({
@@ -40,9 +39,22 @@ const userController = {
         res.sendStatus(400);
       });
   },
-
   createUser({ body }, res) {
     User.create(body)
       .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
       .catch((err) => res.json(err));
   },
