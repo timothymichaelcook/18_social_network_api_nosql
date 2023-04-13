abab3f2e9e2d8e514c516113a0d323ad491b080e
timothymichaelcook <tmc295@email.vccs.edu>
2023-04-12 22:20:23 -0400

    Created getuserbyid function adn create user function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -15,3 +15,34 @@ const userController = {
         res.sendStatus(400);
       });
   },
+
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res
+            .status(404)
+            .json({ message: "No user found with this id!" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
