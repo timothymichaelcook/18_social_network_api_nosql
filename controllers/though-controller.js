1bf214642abc33712af9d18482b56bc279391c16
timothymichaelcook <tmc295@email.vccs.edu>
2023-04-13 11:44:08 -0400

    Created though controller file, finished getallthoughts function

---
+++ controllers/though-controller.js
@@ -0,0 +1,19 @@
+const { Thought, User } = require("../models");
+
+const thoughtController = {
+  // get all Thoughts
+  getAllThought(req, res) {
+    Thought.find({})
+      .populate({
+        path: "reactions",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+};
